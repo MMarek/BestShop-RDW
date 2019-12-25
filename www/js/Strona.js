1bb2f443b8e94288ba35cef6171612f071849790
MMarek <mildnermarek@gmail.com>
2019-12-25 21:53:02 +0100

    header

--- www/js/Strona.js
+++ www/js/Strona.js
@@ -30,11 +30,34 @@ class Navigation extends Component {
     }
 }
 
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="container">
+                    <div className="header-content">
+                        <h1 className="header-main-text">Sell More!</h1>
+                        <p className="header-extra-text">Open shop on our platform and increase your sales</p>
+                        <a href="#" className="header-button">
+                            <button className="header-button-container"><p>Open your shop</p></button>
+                        </a>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+
+
+
+}
+
+
 class App extends Component {
     render() {
         return (
             <>
                 <Navigation/>
+                <Header/>
             </>
         )
     }
