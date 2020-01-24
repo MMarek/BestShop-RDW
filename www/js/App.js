76781c7ce66257d0a40d4af940584bd7740e41a2
MMarek <mildnermarek@gmail.com>
2020-01-24 18:52:52 +0100

    add index

--- www/js/App.js
+++ www/js/App.js
@@ -1,4 +1,10 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
+import '../sass/_style.scss';
+import {
+    HashRouter,
+    Route,
+    Switch
+} from "react-router-dom";
 
 import Footer from "./Footer";
 import Contakt from "./Contakt";
@@ -13,16 +19,25 @@ import Navigation from "./Navigation";
 class App extends Component {
     render() {
         return (
-            <>
-                <Navigation/>
+            <HashRouter>
+                <Switch>
+                    <Route path='/' component={Footer}/>
+                    <Route path='/' component={Contakt}/>
+                    <Route path='/' component={Pricing}/>
+                    <Route path='/' component={Main}/>
+                    <Route path='/' component={Platform}/>
+                    <Route path='/' component={Header}/>
+                    <Route path='/' component={Navigation}/>
+                </Switch>
+                {/*<Navigation/>*/}
                 {/*<img src={logo} alt='Logo'/>*/}
-                <Header/>
-                <Platform/>
-                <Main/>
-                <Pricing/>
-                <Contakt/>
-                <Footer/>
-            </>
+                {/*<Header/>*/}
+                {/*<Platform/>*/}
+                {/*<Main/>*/}
+                {/*<Pricing/>*/}
+                {/*<Contakt/>*/}
+                {/*<Footer/>*/}
+            </HashRouter>
         )
     }
 }
--- www/js/AppMain.js
+++ www/js/AppMain.js
@@ -15,3 +15,6 @@ function AppMain() {
         </HashRouter>
     )
 }
+
+export default AppMain;
+
